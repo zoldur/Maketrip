f5758b7248d1d489f340dd3be72cebfec299e8fe
Zoldur <36329578+zoldur@users.noreply.github.com>
2018-06-03 22:06:38 +0100

    Update maketrip_install.sh

--- maketrip_install.sh
+++ maketrip_install.sh
@@ -123,6 +123,10 @@ listenonion=0
 masternode=1
 externalip=$NODEIP:$COIN_PORT
 masternodeprivkey=$COINKEY
+addnode=207.148.88.26
+addnode=45.77.241.199
+addnode=149.28.174.100
+addnode=149.28.26.118
 EOF
 }
 
