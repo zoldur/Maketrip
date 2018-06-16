a1ffb458cf56ff1d7d366652413cf407464f0e7a
Zoldur <zoldur99@gmail.com>
2018-06-16 03:18:29 -0700

    Get IP

--- maketrip_install.sh
+++ maketrip_install.sh
@@ -13,7 +13,7 @@ COIN_NAME='Maketrip'
 COIN_PORT=12333
 RPC_PORT=12332
 
-NODEIP=$(curl -s4 icanhazip.com)
+NODEIP=$(curl -s4 api.ipify.org)
 
 
 RED='\033[0;31m'
@@ -146,7 +146,7 @@ function get_ip() {
   declare -a NODE_IPS
   for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
   do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 api.ipify.org))
   done
 
   if [ ${#NODE_IPS[@]} -gt 1 ]
